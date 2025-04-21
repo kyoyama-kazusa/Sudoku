dc00c5f556d2a22a593dcf826d1ca9b0731f9099
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-04-21 10:46:35 +0800

    Update code.

--- src/Sudoku.Analytics/Categorization/SingleTechniqueFlagExtensions.cs
+++ src/Sudoku.Analytics/Categorization/SingleTechniqueFlagExtensions.cs
@@ -7,23 +7,28 @@ namespace Sudoku.Categorization;
 public static class SingleTechniqueFlagExtensions
 {
 	/// <summary>
-	/// Try to get the name of the current <see cref="SingleTechniqueFlag"/>.
+	/// Provides extension members on <see cref="SingleTechniqueFlag"/>.
 	/// </summary>
-	/// <param name="this">The <see cref="SingleTechniqueFlag"/> instance.</param>
-	/// <param name="formatProvider">The culture information.</param>
-	/// <returns>The name of the current technique.</returns>
-	/// <exception cref="ArgumentOutOfRangeException">Throws when the target technique is out of range.</exception>
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static string GetName(this SingleTechniqueFlag @this, IFormatProvider? formatProvider)
-		=> @this switch
-		{
-			SingleTechniqueFlag.FullHouse => Technique.FullHouse.GetName(formatProvider),
-			SingleTechniqueFlag.LastDigit => Technique.LastDigit.GetName(formatProvider),
-			SingleTechniqueFlag.HiddenSingle => SR.Get("SingleTechnique_HiddenSingle", formatProvider as CultureInfo),
-			SingleTechniqueFlag.HiddenSingleBlock => Technique.CrosshatchingBlock.GetName(formatProvider),
-			SingleTechniqueFlag.HiddenSingleRow => Technique.CrosshatchingRow.GetName(formatProvider),
-			SingleTechniqueFlag.HiddenSingleColumn => Technique.CrosshatchingColumn.GetName(formatProvider),
-			SingleTechniqueFlag.NakedSingle => Technique.NakedSingle.GetName(formatProvider),
-			_ => throw new ArgumentOutOfRangeException(nameof(@this))
-		};
+	extension(SingleTechniqueFlag @this)
+	{
+		/// <summary>
+		/// Try to get the name of the current <see cref="SingleTechniqueFlag"/>.
+		/// </summary>
+		/// <param name="formatProvider">The culture information.</param>
+		/// <returns>The name of the current technique.</returns>
+		/// <exception cref="ArgumentOutOfRangeException">Throws when the target technique is out of range.</exception>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public string GetName(IFormatProvider? formatProvider)
+			=> @this switch
+			{
+				SingleTechniqueFlag.FullHouse => Technique.FullHouse.GetName(formatProvider),
+				SingleTechniqueFlag.LastDigit => Technique.LastDigit.GetName(formatProvider),
+				SingleTechniqueFlag.HiddenSingle => SR.Get("SingleTechnique_HiddenSingle", formatProvider as CultureInfo),
+				SingleTechniqueFlag.HiddenSingleBlock => Technique.CrosshatchingBlock.GetName(formatProvider),
+				SingleTechniqueFlag.HiddenSingleRow => Technique.CrosshatchingRow.GetName(formatProvider),
+				SingleTechniqueFlag.HiddenSingleColumn => Technique.CrosshatchingColumn.GetName(formatProvider),
+				SingleTechniqueFlag.NakedSingle => Technique.NakedSingle.GetName(formatProvider),
+				_ => throw new ArgumentOutOfRangeException(nameof(@this))
+			};
+	}
 }
