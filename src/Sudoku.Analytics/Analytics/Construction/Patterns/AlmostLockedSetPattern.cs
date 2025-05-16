00153f5cd3658ce51e2f2e16ca5dfe3ff50f2595
Kyouyama Kazusa <Kazusa0805@outlook.com>
2025-05-16 14:58:59 +0800

    Update code.

--- src/Sudoku.Analytics/Analytics/Construction/Patterns/AlmostLockedSetPattern.cs
+++ src/Sudoku.Analytics/Analytics/Construction/Patterns/AlmostLockedSetPattern.cs
@@ -157,9 +157,9 @@ public sealed partial class AlmostLockedSetPattern(
 	[Cached]
 	public static ReadOnlySpan<AlmostLockedSetPattern> Collect(in Grid grid)
 	{
-		// INTERCEPTOR_VARIABLE_DECLARATION_BEGIN
+		// VARIABLE_DECLARATION_BEGIN
 		_ = grid is { EmptyCells: var __EmptyCells, BivalueCells: var __BivalueCells, CandidatesMap: var __CandidatesMap };
-		// INTERCEPTOR_VARIABLE_DECLARATION_END
+		// VARIABLE_DECLARATION_END
 
 		// Get all bi-value-cell ALSes.
 		var result = new List<AlmostLockedSetPattern>();
