92c8b11407b1ea5cc2f5d28f40248d9b412af3d2
Kyouyama Kazusa <kazusa0805@outlook.com>
2026-05-16 22:28:31 +0800

    Update code.

--- src/System/Runtime/CompilerServices/UnionAttribute.cs
+++
@@ -1,7 +0,0 @@
-namespace System.Runtime.CompilerServices;
-
-/// <summary>
-/// Indicates the type is a union type.
-/// </summary>
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
-public sealed class UnionAttribute : Attribute;
